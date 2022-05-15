9c68ba6bac08976ca36448e1afe2c5411cfb7d71
andres-condezo <ra.condezomonge@gmail.com>
2022-05-14 19:30:57 -0500

    Update book, person, and rental classes to add a graphical UI

--- book.rb
+++ book.rb
@@ -1,16 +1,17 @@
+require './rental'
+
 class Book
   # accessor get and set method
-  attr_accessor :title, :author, :rentals
+  attr_accessor :title, :author, :rental
 
   # Constructor to initialize the class Person
   def initialize(title, author)
     @title = title
     @author = author
-    @rentals = []
+    @rental = []
   end
 
-  def add_rental(person)
-    @rentals.push(person)
-    person.rentals = self
+  def add_rental(date, person)
+    Rental.new(date, self, person)
   end
 end
--- person.rb
+++ person.rb
@@ -1,28 +1,28 @@
 # Person: A class to define attributes and behaviors
 require './decorators'
+require './rental'
 
 class Person < Nameable
   # accessor get and set method
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rental
   attr_reader :id
 
   # Constructor to initialize the class Person
   def initialize(age, name = 'Unknown', parent_permission = 'true')
     super()
-    @id = "#{Random.rand(1..1000)}-#{name}"
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rentals = []
+    @rental = []
   end
 
   def correct_name
     @name
   end
 
-  def add_rental(book)
-    @rentals.push(book)
-    book.rentals = self
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   def can_use_services?
