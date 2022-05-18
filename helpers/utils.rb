dddbe32717f067765d585cee6d71a8eacb278dbb
andres-condezo <ra.condezomonge@gmail.com>
2022-05-18 08:43:33 -0500

    Add a clear function

---
+++ helpers/utils.rb
@@ -0,0 +1,14 @@
+def clear
+  system('clear')
+  system('cls')
+end
+
+def print_header
+  clear
+  puts 'Welcome to School library App!'
+  puts '-------------------------------'
+end
+
+def print_div
+  puts '----------------------------------'
+end
